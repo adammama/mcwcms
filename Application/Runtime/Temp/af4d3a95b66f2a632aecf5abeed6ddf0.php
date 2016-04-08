91b0194c5c202e8f1ef5343e252d4a07bdc4fd97
mcw <mcwen@sohu.com>
2016-04-08 11:28:34 +0800

    ignore runtime fold

--- Application/Runtime/Temp/af4d3a95b66f2a632aecf5abeed6ddf0.php
+++
@@ -1,3 +0,0 @@
-<?php
-//000000000000a:3:{s:2:"id";s:1:"1";s:4:"name";N;s:9:"cacheInfo";s:128:"<i class="fa fa-clock-o fw"></i> <em>耗时 1 毫秒</em> / <i class="fa fa-database fw"></i> <em>从 MySQL 数据库读取</em>";}
-?>
